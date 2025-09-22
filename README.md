60f53f389fe57e1d878f088cb618a862413fd1bb
Mike-Testut <michael.testut@gmail.com>
2025-09-22 14:45:14 -0700

    start third person character setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# CharacterSetup
+
+Setting up basic third person character and true first person character with c++
+Developed with Unreal Engine 5
---
+++ Source/CharacterSetup/Private/Core/CharacterSetupGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "CharacterSetupGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARACTERSETUP_API ACharacterSetupGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
