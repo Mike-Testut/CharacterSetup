e4c32281c78316a54ee33e65c84261ee1f4bca7d
Mike-Testut <michael.testut@gmail.com>
2025-10-04 18:05:26 -0700

    start changing anim bp to c++

---
+++ Source/CharacterSetup/Private/Animation/ThirdPersonAnimInstance.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ThirdPersonAnimInstance.h"
+
+void UThirdPersonAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+}
+
+void UThirdPersonAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+}
---
+++ Source/CharacterSetup/Private/Animation/ThirdPersonAnimInstance.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ThirdPersonAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+class AThirdPersonCharacter;
+/**
+ * 
+ */
+UCLASS()
+class CHARACTERSETUP_API UThirdPersonAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	AThirdPersonCharacter* ThirdPersonCharacter;
+
+	UPROPERTY()
+	UCharacterMovementComponent* 
+	
+};
